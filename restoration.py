bba1a4d8b8b0b6db327089818d448ad16bce7ed1
Sawaki Shuto <swkshuto0208@icloud.com>
2023-11-23 12:00:59 +0900

    固有値やベクトルから逆算する

---
+++ restoration.py
@@ -0,0 +1,13 @@
+# AVV^t = V /\_ V^T
+import numpy as np
+def restration(A, V):
+    return V @ A @ V.T
+
+
+# 固有値の対角要素
+A = np.diag([2, 3])
+print("固有値の対角要素は\n{}".format(A))
+# 固有ベクトル
+V = np.array([[1/np.sqrt(2), 1/np.sqrt(2)],[1/np.sqrt(2), -1/np.sqrt(2)]])
+print("固有ベクトルは\n{}".format(V))
+print("復元結果は\n{}".format(restration(A,V)))
