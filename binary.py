caabcbed46c949ab23fdf14b5f27a5851167dde8
Sawaki Shuto <swkshuto0208@icloud.com>
2023-11-24 11:56:41 +0900

    文言修正

--- binary.py
+++ binary.py
@@ -6,19 +6,18 @@ def f(x):
     return x**2 -5.0
 
 def binarySearch(f, a, b, limit=2.23, max=50):
-    num = 0  
+    num = 0
     print("{} {}<=x<={}".format(num, a, b))
-
     if(f(a)*f(b) > 0):
         return
     while(True):
         c = (b+a)/2.0
         if (0.0 < f(c)*f(a)):  
             a = c  
-        else: 
+        elif(f(b) * f(c) > 0): 
             b = c 
         num += 1
-        print("{}  {}<=x<={}より".format(num, a, b))
+        print("{}回目は a ={}, b = {}より".format(num, a, b))
         if((b-a >= limit) or max <= num):
             break
     print(str(c))
--- poewr.py
+++ poewr.py
@@ -1,20 +1,21 @@
 # 冪乗で固有値計算
 import numpy as np
 
-def power(matrix, v, limit=0.1, max_iter=100):
+def power(matrix, v, limit=0.001, max_iter=100):
     n = 0
     while n < max_iter:
         x = matrix @ v 
-        r = x[0, 0] 
-        print(f"反復 {n + 1}  固有値: {r}")
+        r = x[0,0]
+        print(f"反復 {n + 1}  固有値r: {r}")
         v = x / r 
         n += 1
+        print("x{}の値は\n{}".format(n,v))
         if np.linalg.norm(matrix @ v - r * v) < limit:
             print("収束した！")
             return r, v   
     return None
 # 対象の行列
-matrix_A = np.array([[3, 1], [1, 3]])
+matrix_A = np.array([[4, 1], [1, 0]])
 # 初期値
 initialmat = np.array([[1], [0]])
 power(matrix_A, initialmat)
